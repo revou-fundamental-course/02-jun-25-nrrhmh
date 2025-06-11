38efc604e3872b178e8d8de5be8bad5c815cb848
nrrhmh <109803740+nrrhmh@users.noreply.github.com>
2025-06-12 05:54:55 +0700

    Update Project Revo U.js

--- Project Revo U.js
+++ Project Revo U.js
@@ -1,17 +1,18 @@
 document.getElementById("messageForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-  
-    const name = document.getElementById("name").value;
-    const dob = document.getElementById("dob").value;
-    const gender = document.getElementById("gender").value;
-    const message = document.getElementById("messageText").value;
-  
-    const currentTime = new Date().toLocaleString();
-  
-    document.getElementById("currentTime").textContent = currentTime;
-    document.getElementById("outputName").textContent = name;
-    document.getElementById("outputDob").textContent = dob;
-    document.getElementById("outputGender").textContent = gender;
-    document.getElementById("outputMessage").textContent = message;
-  });
-  
+  e.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const dob = document.getElementById("dob").value;
+  const gender = document.getElementById("gender").value;
+  const message = document.getElementById("messageText").value;
+
+  const currentTime = new Date().toLocaleString();
+
+  document.getElementById("currentTime").textContent = currentTime;
+  document.getElementById("outputName").textContent = name;
+  document.getElementById("outputDob").textContent = dob;
+  document.getElementById("outputGender").textContent = gender;
+  document.getElementById("outputMessage").textContent = message;
+
+  alert("Terima kasih, pesan Anda telah dikirim!");
+});
